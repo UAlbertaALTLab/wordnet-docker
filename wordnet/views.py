c23741a21f753d22c7eb1031c0e4c51d0c53b028
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2024-09-03 16:46:17 +0000

    factored out search for extra features

--- wordnet/views.py
+++ wordnet/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from . import nltk
 
 def index(request):
-    search = request.GET["search"] if request.method == "GET" else ""
+    search = request.GET.get("search", "")   if request.method == "GET" else ""
 
     context = {
         "wordnet": "We want to see the wizard of oz!",
         "search": search,
-        "results": nltk.search(search)
+        "results": results(search)
     }
     return render(request, "wordnet/index.html", context)
+
+def results(search):
+    return nltk.search(search) if search else []
