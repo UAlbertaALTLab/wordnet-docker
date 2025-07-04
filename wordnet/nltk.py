d12290b6e5ab6f494d697a954cc928c98e956f99
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2025-07-04 21:49:32 +0000

    Updates for toolbox work

--- wordnet/nltk.py
+++ wordnet/nltk.py
@@ -1,5 +1,6 @@
 from nltk.corpus import wordnet as wn
 from nltk.corpus.reader.wordnet import Synset
+from itertools import groupby
 import re
 
 format_regexp = r'^\s*\((?P<pos>\w+)\)\s+(?P<stem>[^\s]+)\s*\#\s*(?P<num>\d+)\s*\Z'
@@ -21,15 +22,33 @@ def name_format(synset : Synset):
     entry = ".".join(data[0:-2])
     return f"({data[-2]}) {entry}#{int(data[-1])}"
 
+wordnet_keys = dict(v='v.', n='n.', a='a.', r='adv.', s='a.')
+
+def name_format_toolbox(synset : Synset, new_index, new_key):
+    pos = wordnet_keys[synset.pos()]
+    if new_index is None or new_key is None:
+        data = synset.name().split(".")
+        entry = " ".join(".".join(data[0:-2]).split("_"))
+        return f"{entry} {pos} #{data[-1]}"
+    return f"{new_key} {pos} #{int(new_index+1)}"
+
 def normalize_keyword(keyword: str) -> str:
     matches = re.match(format_regexp, keyword)
     if matches:
         return matches['stem']+'.'+matches['pos']+'.'+matches['num']
-    return keyword
+    return '_'.join(keyword.split())
 
-def normalize_result(result):
+def reverse_normalized_keyword(keyword: str) -> str:
+    matches = re.match(format_regexp, keyword)
+    if matches:
+        return ' '.join(matches['stem'].split('_'))
+    return ' '.join(keyword.split('_'))
+
+
+def normalize_result(result, new_index = None, new_key = None):
     return {
         "name" : name_format(result),
+        "toolbox_name" : name_format_toolbox(result, new_index, new_key), 
         "pos"  : result.pos,
         "definition" : result.definition,
         "lemmas" : [lemma.name for lemma in result.lemmas()],
@@ -39,4 +58,7 @@ def normalize_result(result):
     }
 
 def search(keyword):
-    return [normalize_result(result) for result in candidates(keyword)]
+    entry = reverse_normalized_keyword(keyword)
+    results = groupby(candidates(keyword), key = lambda x : x.name().split('.')[-2])
+    answers = [normalize_result(result, index, entry) for _, grouped in results for index, result in enumerate(grouped)]
+    return enumerate(answers)
